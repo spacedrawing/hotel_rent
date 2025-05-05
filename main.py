110887915a95d80d1faffebf682526041bced0ed
Space Drawing <kulevojdanil@gmail.com>
2025-05-05 20:28:29 +0300

    Add hotel menu page and update index with hotel cards linking to it

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/hotel_menu")
+def hotel_menu():
+    return render_template("hotel_menu.html")   
+
 def main():
     app.run(host="127.0.0.1", port=8080, debug=True)
 
