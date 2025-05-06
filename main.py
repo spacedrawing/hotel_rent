9604fe2c70a002224769eeb3bd15ff604f6ce782
Space Drawing <kulevojdanil@gmail.com>
2025-05-06 19:55:22 +0300

    add hotel_menu and pictures

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def index():
 
 @app.route("/hotel_menu")
 def hotel_menu():
-    return render_template("hotel_menu.html")   
+    images = [
+        url_for('static', filename='images/room1.png'),
+        url_for('static', filename='images/room2.jpg'),
+        url_for('static', filename='images/room3.jpg'),
+    ]
+    return render_template("hotel_menu.html", images=images)
 
 def main():
     app.run(host="127.0.0.1", port=8080, debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,34 @@
-const filterItems = document.querySelectorAll(".filters ul li");
-
-filterItems.forEach((item) => {
-    item.onclick = () => {
-        filterItems.forEach((el) => el.classList.remove("active"));
-        item.classList.add("active");
-    };
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const images = window.images;
+    
+    let currentImageIndex = 0;
+    const imageElement = document.getElementById('carousel-image');
+  
+    function updateImage() {
+      imageElement.src = images[currentImageIndex];
+      console.log("Current Image:", images[currentImageIndex]);
+    }
+  
+    function nextImage() {
+      if (currentImageIndex < images.length - 1) {
+        currentImageIndex++;
+      } else {
+        currentImageIndex = 0;
+      }
+      updateImage();
+    }
+  
+    function prevImage() {
+      if (currentImageIndex > 0) {
+        currentImageIndex--;
+      } else {
+        currentImageIndex = images.length - 1;
+      }
+      updateImage();
+    }
+  
+    document.querySelector('.carousel-btn.next').addEventListener('click', nextImage);
+    document.querySelector('.carousel-btn.prev').addEventListener('click', prevImage);
+  
+    updateImage();
+  });
