c41c5058eadd34bc84ccf1c9d9a3f9323c8838cc
Space Drawing <kulevojdanil@gmail.com>
2025-05-08 16:46:42 +0300

    add Rview model

---
+++ data/review.py
@@ -0,0 +1,13 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+class Review(SqlAlchemyBase):
+    __tablename__ = 'review'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True, default='')
+    rating = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, default=0)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("user.id"))
+    user = orm.relationship("User", back_populates='reviews')
+    hotel_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("hotel.id"))
+    hotel = orm.relationship("Hotel", back_populates='reviews')
