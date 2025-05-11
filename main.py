fb502997de6f677e028ff94b79c8a3500b5fe6db
Kapi <michaelpo88@mail.ru>
2025-05-11 09:45:00 +0300

    Adding different pictures to "hotel_menu" collecting up-to-date information about reviews

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from data import db_session
 from flask import Flask, render_template, url_for
 from data.hotel import Hotel
+from data.review import Review
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -10,18 +11,25 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route("/")
 def index():
-    list_hotel_cards = Hotel()
-    list_hotel_cards = [json.loads(i) for i in open('data/hotel_cards.txt', encoding="utf-8").read().split('\n')]
+    db_sess = db_session.create_session()
+    list_hotel_cards = [i.__dict__ for i in db_sess.query(Hotel).all()]
+
+    for slov in list_hotel_cards:
+        reviews = [i.__dict__ for i in db_sess.query(Review).filter(Review.hotel_id == slov["id"]).all()]
+        if len(reviews) == 0:
+            slov["rating"] = "Нет отзывов"
+        else:
+            slov["rating"] = str(sum(i["rating"] for i in reviews) / len(reviews))
 
     return render_template("index.html", list_hotel_cards=list_hotel_cards)
 
 
-@app.route("/hotel_menu")
-def hotel_menu():
-    files = os.listdir(path="static/images")
-    images = [url_for('static', filename=f'images/room{i}.jpg') for i in range(1, len(files) + 1)]
+@app.route("/hotel_menu/<index_hotel>")
+def hotel_menu(index_hotel):
+    files = os.listdir(path="static/images_1")
+    images = [url_for('static', filename=f'images_{index_hotel}/room{i}.jpg') for i in range(1, len(files) + 1)]
 
-    return render_template("hotel_menu.html", images=images)
+    return render_template("hotel_menu.html", images=images, index_hotel=index_hotel)
 
 
 def main():
@@ -31,5 +39,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    db_sess = db_session.create_session()
